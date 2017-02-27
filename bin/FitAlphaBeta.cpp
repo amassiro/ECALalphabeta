c4be9b9379ecaec3c03625cd8215cedbcae017eb
amassiro <massironi.andrea@gmail.com>
2017-02-27 11:31:07 +0100

    first import

---
+++ bin/FitAlphaBeta.cpp
@@ -0,0 +1,26 @@
+
+//---- std include ----
+#include <string>
+#include <map>
+#include <vector>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <cstdlib>
+#include <fstream> 
+
+
+
+int main(int argc, char** argv) {
+  
+  std::cout << " AlphaBeta fit " << std::endl;
+  
+  std::cout << std::endl;
+  
+}
+
+
+
+  
